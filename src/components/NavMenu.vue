44e726d88ca8f68bed14aca659cd0e7151701693
Evgeny <zasetsky.evgeny@gmail.com>
2023-07-10 14:07:21 +0300

    first styles

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <el-menu class="el-menu-vertical-demo" :collapse="isCollapsed">
-      <el-menu-item @click="isCollapsed = !isCollapsed">
-        <i>
+  <div class="menu">
+    <el-menu class="menu__list" :collapse="isCollapsed">
+      <el-menu-item class="menu__item" @click="isCollapsed = !isCollapsed">
+        <i class="menu__icon-company">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="32"
@@ -26,33 +26,35 @@
             />
           </svg>
         </i>
-        <span v-show="!isCollapsed">КОМПАНИЯ</span>
+        <span class="menu__title" v-show="!isCollapsed">КОМПАНИЯ</span>
       </el-menu-item>
-      <el-menu-item index="2">
-        <i class="el-icon-time"></i>
-        <span v-show="!isCollapsed">График работы</span>
+      <el-menu-item class="menu__item" index="1">
+        <i class="menu__icon el-icon-time"></i>
+        <span class="menu__label" v-show="!isCollapsed">График работы</span>
       </el-menu-item>
-      <el-menu-item index="3"
-        ><i class="el-icon-news"></i>
-        <span v-show="!isCollapsed">Новости Компании</span></el-menu-item
-      >
-      <el-menu-item index="3">База знаний</el-menu-item>
-      <el-menu-item index="4">Пароли</el-menu-item>
-      <el-submenu index="5">
-        <template v-slot:title>Настройки</template>
-        <el-menu-item index="5-1">Подменю 1</el-menu-item>
-        <el-menu-item index="5-2">Подменю 2</el-menu-item>
-        <el-menu-item index="5-3">Подменю 3</el-menu-item>
-      </el-submenu>
+      <el-menu-item class="menu__item" index="2">
+        <i class="menu__icon el-icon-news"></i>
+        <span class="menu__label" v-show="!isCollapsed">Новости компании</span>
+      </el-menu-item>
+      <el-menu-item class="menu__item" index="3">
+        <i class="menu__icon"><Icon icon="tabler:book" /></i>
+        <span class="menu__label" v-show="!isCollapsed">База знаний</span>
+      </el-menu-item>
+      <el-menu-item class="menu__item" index="4">
+        <i class="menu__icon"><Icon icon="iconoir:password-pass" /></i>
+        <span class="menu__label" v-show="!isCollapsed">Пароли</span>
+      </el-menu-item>
     </el-menu>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import { Icon } from "@iconify/vue";
 
 export default defineComponent({
-  name: "NavMenu",
+  components: { Icon },
+
   setup() {
     const isCollapsed = ref(false);
     return { isCollapsed };
@@ -60,12 +62,40 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-  min-height: 400px;
-}
-.collapse-btn {
-  margin-bottom: 20px;
+<style scoped lang="scss">
+.menu {
+  &__list:not(.el-menu--collapse) {
+    width: 264px;
+    height: 1024px;
+  }
+
+  &__list {
+    height: 1024px;
+  }
+
+  &__item {
+  }
+
+  &__title {
+    font-weight: 700;
+    font-size: 24px;
+    margin-left: 5px;
+    color: $text-color-nav-title;
+  }
+
+  &__icon {
+    font-size: 24px !important;
+    line-height: 20px;
+  }
+
+  &__icon-company {
+    margin-top: -10px;
+  }
+
+  &__label {
+    line-height: 25px;
+    margin-left: 10px;
+    color: $text-color-nav;
+  }
 }
 </style>
