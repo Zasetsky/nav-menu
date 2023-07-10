ffd885d71f3b43437d66bf4510daaabf9f09deb0
Evgeny <zasetsky.evgeny@gmail.com>
2023-07-10 11:20:43 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
+import router from "./router/index";
+import ElementPlus from "element-plus";
 
-createApp(App).use(store).mount("#app");
+import "element-plus/dist/index.css";
+import "@/assets/theme/index.css";
+
+createApp(App).use(store).use(ElementPlus).use(router).mount("#app");
