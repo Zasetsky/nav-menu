db5113ec801f69daf53e19a12b1123bba6c5ea96
Evgeny <zasetsky.evgeny@gmail.com>
2023-07-21 11:36:16 +0300

    some fixes

--- src/views/WorkSchedule.vue
+++ src/views/WorkSchedule.vue
@@ -12,8 +12,8 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import DepartmentList from "@/components/work-schedule.components/DepartmentList.vue";
-import CalendarHeader from "@/components/work-schedule.components/CalendarHeader.vue";
+import DepartmentList from "@/components/work-schedule.components/department/DepartmentList.vue";
+import CalendarHeader from "@/components/work-schedule.components/calendar/CalendarHeader.vue";
 
 export default defineComponent({
   components: { DepartmentList, CalendarHeader },
