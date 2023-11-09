84a9f08d885b77f9aac63917dd032e06686d543b
Evgeny <zasetsky.evgeny@gmail.com>
2023-11-09 11:06:23 +0300

    square session buttons

--- src/components/work-session/WorkSessionButton.vue
+++ src/components/work-session/WorkSessionButton.vue
@@ -367,6 +367,10 @@ export default defineComponent({
   }
 
   &__button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
     height: 40px;
     border: none;
     cursor: pointer;
